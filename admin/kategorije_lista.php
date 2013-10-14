8f1cddba628cf2a362772d741c189dd9228effb6
avramz <php88.sd@gmail.com>
2013-10-14 23:00:19 +0200

    kategorije_lista fix

--- admin/kategorije_lista.php
+++ admin/kategorije_lista.php
@@ -36,15 +36,9 @@
                             <?php
                             include './logic/common.php';
                             $query = '
-<<<<<<< HEAD
+
 								SELECT distinct kategorije.naziv_kategorije as ime, (select count(*) fROM oglas o WHERE o.kategorija = kategorije.id_kategorije) as broj_oglasa from kategorije
-=======
-								SELECT DISTINCT k.naziv_kategorije AS ime, COUNT(o.kategorija) AS broj_oglasa
-								FROM kategorije k
-								LEFT JOIN oglas o ON o.kategorija = k.id_kategorije
-								GROUP BY k.naziv_kategorije
-								ORDER BY k.naziv_kategorije ASC
->>>>>>> 8cbfa40f570e065d2145d9a9b8fee0cb20460b73
+
 							';
 								
 
