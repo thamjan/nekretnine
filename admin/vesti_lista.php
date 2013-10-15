dbd95051880462270f621ae51c2ba32804783000
thamjan <thamjan@yahoo.com>
2013-10-16 00:00:29 +0200

    vesti lista - skica...

--- admin/vesti_lista.php
+++ admin/vesti_lista.php
@@ -14,8 +14,81 @@
 
                     <div class="full_w">
                         <div class="h_title">Lista vesti</div>
+						
+					</div>
 
-                    </div>			
+					<div class="half_w half_left">
+						<div class="h_title">Treba da se doda tema vesti..........</div>
+						<article>
+							<h4>25. april 2013.</h4>
+							<p>tema vesti i u bazi i na strani za dodavanje...</p>
+							<p><em>ovde nećemo previše da se zezamo oko stilizovanja, pretpostavljam..</em></p>
+							<ul>
+								<li>tako da ću odraditi samo ovako osnovno prikazivanje...&nbsp;</li>
+							</ul>
+							<p><strong>treba i ovde tasteri za brisanje/editovanje?</strong></p>
+							<p>pobegoh u vetkre... čujemo se sutra...&nbsp;</p>
+							<p>signin' offf........</p>
+							<h5>autor: Damjan...</h5>
+						</article>
+					</div>
+			
+					<div class="half_w half_right">
+						<div class="h_title">Vest 2</div>
+						<article>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p><em>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</em></p>
+							<ul>
+								<li>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</li>
+							</ul>
+							<p><strong>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</strong></p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p><em>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</em></p>
+							<ul>
+								<li>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</li>
+							</ul>
+							<p><strong>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</strong></p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p>end</p>
+						</article>
+					</div>
+					
+					<div class="half_w half_left">
+						<div class="h_title">Vest 2</div>
+						<article>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p><em>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</em></p>
+							<ul>
+								<li>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</li>
+							</ul>
+							<p><strong>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</strong></p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p><em>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</em></p>
+							<ul>
+								<li>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</li>
+							</ul>
+							<p><strong>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</strong></p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p>end</p>
+						</article>
+					</div>
+					
+					<div class="half_w half_right">
+						<div class="h_title">Vest 1</div>
+						<article>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p><em>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</em></p>
+							<ul>
+								<li>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</li>
+							</ul>
+							<p><strong>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</strong></p>
+							<p>tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst tekst tek te teksttt ke tek.. tekst&nbsp;</p>
+							<p>end</p>
+						</article>
+					</div>
+						
                 </div>	
             </div>
 
